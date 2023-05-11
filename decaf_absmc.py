d5efcd317a5281a2dd3382611b393a7f9ce2e900
Kah Shiuh Tang <107123805+kahshiuhtang@users.noreply.github.com>
2023-05-11 03:37:56 +0000

    work

--- decaf_absmc.py
+++ decaf_absmc.py
@@ -1,45 +1,61 @@
 class Register():
-    def __init__(self, name, type):
-        self.name = ""
+    def __init__(self):
+        self.name = None
         self.type = None
 class Temp(Register):
     def __init__(self, name):
-        super.__init__(name, "Temp")
+        self.name = name
+        self.type = "Temp"
+    def __str__(self):
+        return self.name
 class Arg(Register):
     def __init__(self, name):
-        super.__init__(name, "Arg")
+        self.name = name
+        self.type = "Arg"
+    def __str__(self):
+        return self.name
+class Label():
+    def __init__(self):
+        pass
 class Machine():
     def __init__(self, ast):
         self.args = []
         self.args_status = []
         self.temps = []
         self.temps_status = []
-        self.heap = []
-        self.control = []
-        self.data = []
         self.code = []
+        self.labels = 0
         self.ast = ast
-    def addLine(self, inst):
+    def capp(self, inst):
         self.code.append(inst)
     def arg_reg(self):
         for i in range(len(self.args)):
             if self.args_status[i]:
                 self.args_status[i] = False
                 return self.args[i]
-        return new_reg("arg")
+        return self.new_reg("arg")
 
     def temp_reg(self):
         for i in range(len(self.args)):
             if self.temp_status[i]:
                 self.temp_status[i] = False
                 return self.temp[i]
-        return new_reg("temp")
-    
+        return self.new_reg("temp")
+    def label(self):
+        self.labels += 1
+        return "L" + str(self.labels) + ":"
     def new_reg(self, typ):
         stat = self.args_status if typ == "arg" else self.temp_status
-        regs = self.args if typ == "arg" else self.Temp
+        regs = self.args if typ == "arg" else self.temp
         new_reg = Arg("a" + str(len(self.args))) if typ == "arg" else Temp("t" + str(len(self.temp)))
         stat.append(True)
+        return new_reg
+
+    def clean(self):
+        self.args = []
+        self.args_status = []
+        self.temp = []
+        self.temp_status = []
 
 class Instructions():
     def __init__(self, name):
@@ -71,173 +87,238 @@ class move_immed_i(Move):
         super().__init__("move_immed_i")
         self.r = r
         self.i = i
+    def __str__(self):
+        return self.name + " " + self.r + " " + str(self.i)
 class move_immed_f(Move):
     def __init__(self,r,i):
         super().__init__("move_immed_f")
         self.r = r
         self.i = i
+    def __str__(self):
+        return self.name + " " + self.r + " " + str(self.i)
 class move(Move):
     def __init__(self,r1,r2):
         super().__init__("move")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class iadd(IntArith):
     def __init__(self,r1,r2,r3):
         super().__init__("iadd")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class isub(IntArith):
     def __init__(self,r1,r2,r3):
         super().__init__("isub")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class imul(IntArith):
     def __init__(self,r1,r2,r3):
         super().__init__("imul")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class idiv(IntArith):
     def __init__(self,r1,r2,r3):
         super().__init__("idiv")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class imod(IntArith):
     def __init__(self,r1,r2,r3):
         super().__init__("imod")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class igt(IntArith): 
     def __init__(self,r1,r2,r3):
         super().__init__("igt")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class igeq(IntArith):
     def __init__(self,r1,r2,r3):
         super().__init__("igeq")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class ilt(IntArith):
     def __init__(self,r1,r2,r3):
         super().__init__("ilt")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class ileq(IntArith):
     def __init__(self,r1,r2,r3):
         super().__init__("ileq")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class fadd(FloatArith):
     def __init__(self,r1,r2,r3):
         super().__init__("fadd")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class fsub(FloatArith):
     def __init__(self,r1,r2,r3):
         super().__init__("fsub")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class fmul(FloatArith):
     def __init__(self,r1,r2,r3):
         super().__init__("fmul")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class fdiv(FloatArith):
     def __init__(self,r1,r2,r3):
         super().__init__("fdiv")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class fgt(FloatArith): 
     def __init__(self,r1,r2,r3):
         super().__init__("fgt")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class fgeq(FloatArith):
     def __init__(self,r1,r2,r3):
         super().__init__("fgeq")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class flt(FloatArith):
     def __init__(self,r1,r2,r3):
         super().__init__("flt")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class fleq(FloatArith):
     def __init__(self,r1,r2,r3):
         super().__init__("fleq")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class ftoi(Conversions):
     def __init__(self,r1,r2):
         super().__init__("ftoi")
         self.r1 = r1
         self.r2 = r2
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 
 class itof(Conversions):
     def __init__(self,r1,r2):
         super().__init__("itof")
         self.r1 = r1
         self.r2 = r2
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 
 class bz(Branches):
     def __init__(self,r1,L):
         super().__init__("bz")
         self.r1 = r1
         self.L = L
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.L
 class bnz(Branches):
     def __init__(self,r1,L):
         super().__init__("bnz")
         self.r1 = r1
         self.L = L
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.L
 class jmp(Branches):
     def __init__(self,L):
         super().__init__("jmp")
         self.L = L
+    def __str__(self):
+        return self.name + " " + self.L
 class hload(HeapMan):
     def __init__(self,r1,r2,r3):
         super().__init__("hload")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class hstore(HeapMan):
     def __init__(self,r1,r2,r3):
         super().__init__("hstore")
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 + " " + self.r3
 class halloc(HeapMan):
     def __init__(self,r1,r2):
         super().__init__("")
         self.r1 = r1
         self.r2 = r2
+    def __str__(self):
+        return self.name + " " + self.r1 + " " + self.r2 
 class call(Proced):
     def __init__(self,L):
         super().__init__("")
         self.L = L
+    def __str__(self):
+        return self.name + " " + self.L
 class ret(Proced):
     def __init__(self):
         super().__init__("")
+    def __str__(self):
+        return self.name
 class save(Proced):
     def __init__(self,r):
         super().__init__("")
+    def __str__(self):
+        return self.name
 class store(Proced):
     def __init__(self,r):
         super().__init__("")
+        self.r = r
+    def __str__(self):
+        return self.name + " " + self.r
 
 
 
--- decaf_codegen.py
+++ decaf_codegen.py
@@ -1,5 +1,5 @@
 import decaf_absmc as mc
-
+import decaf_ast as ast
 class CodeGenerator():
     def __init__(self, ast):
         self.ast = ast
@@ -9,11 +9,120 @@ class CodeGenerator():
         static_fields = 0
         for c in self.ast.class_dict.values():
             for var in c.fields:
-                if var.applicability == "static":
+                if c.fields.get(var).applicability == "static":
                     static_fields += 1
         if static_fields != 0:
-            self.machine.addLine()
+            self.machine.capp("")
         for meth in self.ast.methods.values():
+            self.machine.clean()
+            args = {}
+            temps = {}
+            for var in meth.variable_table:
+                if var.kind == "local":
+                    reg = self.machine.temp_reg()
+                    temps.update({var.id: reg})
+                else:
+                    reg = self.machine.arg_reg()
+                    args.update({var.id: reg})
             for stmt in meth.body.expressions:
-                if isinstance(stmt, list):
-                    
+                self.write(stmt, args, temps, meth)
+        for s in self.machine.code:
+            print(s)
+
+    def write(self, stmt, args, temp, meth):
+        if isinstance(stmt, list):
+            pass
+        elif isinstance(stmt, ast.If):
+            self.machine.capp(self.machine.label()) 
+            self.write(stmt.condition, args, temp, meth)
+        elif isinstance(stmt, ast.While):
+            pass
+        elif isinstance(stmt, ast.For):
+            pass
+        elif isinstance(stmt, ast.Return):
+            pass
+        elif isinstance(stmt, ast.Block):
+            pass
+        elif isinstance(stmt, ast.Break):
+            pass
+        elif isinstance(stmt, ast.Continue):
+            pass
+        elif isinstance(stmt, ast.Skip):
+            pass
+        elif isinstance(stmt, ast.Expression):
+            if isinstance(stmt, ast.ConstantExpression):
+                reg = self.machine.temp_reg()
+                if stmt.type == "int":
+                    self.machine.capp(mc.move_immed_i(reg.name, stmt.value))
+                elif stmt.type == "float":
+                    self.machine.capp(mc.move_immed_i(reg.name, stmt.value))
+                elif stmt.value == "false":
+                    self.machine.capp(mc.move_immed_i(reg.name, 0))
+                else:
+                    self.machine.capp(mc.move_immed_i(reg.name, 1))
+                return reg
+            elif isinstance(stmt, ast.VarExpression):
+                pass
+            elif isinstance(stmt, ast.UnaryExpression):
+                pass
+            elif isinstance(stmt, ast.BinaryExpression):
+                r1 = self.write(stmt.left_operand,args, temp, meth)
+                r2 = self.write(stmt.right_operand, args, temp, meth)
+                r3 = self.machine.temp_reg()
+                flt = False
+                if stmt.left_operand.type == "float" or stmt.right_operand.type == "float":
+                    flt = True
+                if stmt.operator == "add":
+                    if flt:
+                        ast.machine.capp(mc.fadd(r3,r1,r2))
+                    else:
+                        ast.machine.capp(mc.iadd(r3,r1,r2))
+                elif stmt.operator == "min":
+                    if flt:
+                        ast.machine.capp(mc.fsub(r3,r1,r2))
+                    else:
+                        ast.machine.capp(mc.isub(r3,r1,r2))
+                elif stmt.operator == "div":
+                    if flt:
+                        ast.machine.capp(mc.fdiv(r3,r1,r2))
+                    else:
+                        ast.machine.capp(mc.idiv(r3,r1,r2))
+                elif stmt.operator == "mul":
+                    if flt:
+                        ast.machine.capp(mc.fmul(r3,r1,r2))
+                    else:
+                        ast.machine.capp(mc.imul(r3,r1,r2))
+                elif stmt.operator == "or":
+                    ast.machine.capp(mc.fmul(r3,r1,r2))
+                elif stmt.operator == "and":
+
+                elif stmt.operator == "eq":
+           
+                elif stmt.operator == "neq":
+                
+                elif stmt.operator == "lt":
+              
+                elif stmt.operator == "gt":
+         
+                elif stmt.operator =="leq":
+ 
+                elif stmt.operator == "geq":
+
+            elif isinstance(stmt, ast.AssignExpression):
+                pass
+            elif isinstance(stmt, ast.AutoExpression):
+                pass
+            elif isinstance(stmt, ast.FieldAccessExpression):
+                pass
+            elif isinstance(stmt, ast.MethodCallExpression):
+                pass
+            elif isinstance(stmt, ast.NewObjectExpression):
+                pass
+            elif isinstance(stmt, ast.ThisExpression):
+                pass
+            elif isinstance(stmt, ast.SuperExpression):
+                pass
+            elif isinstance(stmt, ast.ClassReferenceExpression):
+                pass  
+        else:
+            print("HMMM, unknown type" + stmt.__str__())    
